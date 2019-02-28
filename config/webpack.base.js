c38313a1e8db62d89f3918d6b316557b3950515d
mateuszszmytko <mateusz.szmytko@gmail.com>
2019-03-01 00:55:15 +0100

    change typescript loader

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -24,7 +24,7 @@ module.exports = {
 
     module: {
         rules: [
-            { test: /\.ts?$/, use: "awesome-typescript-loader" },
+            { test: /\.ts?$/, use: "ts-loader" },
             {
 				test: /\.html$/,
 				loader: 'html-loader?attrs[]=img:src&attrs[]=video:src&attrs[]=source:src'
